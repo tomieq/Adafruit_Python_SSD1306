945d184ecb3074ae20a1e25bc6b2c2d2c642629f
tomieq <developer@tomieq.com>
2019-03-19 11:32:47 +0100

    Delete "Load" text

--- examples/stats.py
+++ examples/stats.py
@@ -112,7 +112,7 @@ while True:
     HOSTNAME =  subprocess.check_output(cmd, shell = True)
     cmd = "hostname -I | cut -d\' \' -f1"
     IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell = True )
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
     MemUsage = subprocess.check_output(cmd, shell = True )
